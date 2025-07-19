d5e2a674fe438ca41cc0a5160d6a0e1126610730
Nguyen <hnguyen0494@gmail.com>
2025-07-19 15:35:39 +0700

    Lan45

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,12 +1,14 @@
 from telegram import Bot
 import asyncio
+import os
 
 async def send_telegram(photo_path="alert.png"):
     try:
-        my_token = "7803495830:AAFOIrci94vNMZGGcYoi2u9n4wFw8HjAwrA"
+        my_token = os.getenv("TELEGRAM_BOT_TOKEN")
+        chat_id = os.getenv("TELEGRAM_CHAT_ID")
         bot = Bot(token=my_token)
         with open(photo_path, "rb") as photo:
-            await bot.send_photo(chat_id="1823943673", photo=photo, caption="Có xâm nhập, nguy hiểm!")
+            await bot.send_photo(chat_id=chat_id, photo=photo, caption="Có xâm nhập, nguy hiểm!")
     except Exception as ex:
         print("Không thể gửi Telegram:", ex)
     else:
