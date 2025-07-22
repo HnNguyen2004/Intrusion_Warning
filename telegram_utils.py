be9eb9116694190d642a83794af7080049cae7c5
Đinh Văn Phi <dinhvanphi01478965@gmail.com>
2025-07-22 21:31:51 +0700

    cau hinh env

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,12 +1,18 @@
 from telegram import Bot
 import asyncio
+import os
+from dotenv import load_dotenv  # <- Thêm dòng này
+
+load_dotenv()  # <- Nạp biến môi trường từ file .env
 
 async def send_telegram(photo_path="alert.png"):
     try:
-        my_token = "7803495830:AAFOIrci94vNMZGGcYoi2u9n4wFw8HjAwrA"
+        my_token = os.getenv("TELEGRAM_BOT_TOKEN")  # <- Dùng os.getenv()
+        chat_id = os.getenv("TELEGRAM_CHAT_ID")
+        
         bot = Bot(token=my_token)
         with open(photo_path, "rb") as photo:
-            await bot.send_photo(chat_id="1823943673", photo=photo, caption="Có xâm nhập, nguy hiểm!")
+            await bot.send_photo(chat_id=chat_id, photo=photo, caption="Có xâm nhập, nguy hiểm!")
     except Exception as ex:
         print("Không thể gửi Telegram:", ex)
     else:
